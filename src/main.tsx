65370c0771c176f1bacde85087fe8e6c74560c11
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-05-21 18:05:18 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from '../src/components/App/App.tsx'
+import App from './components/App/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
