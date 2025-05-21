b7ce31ad9f5a36577295360418f4e3031275b7b4
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-05-21 15:43:57 +0200

    update

--- src/Components/VoteOptions/VoteOptions.tsx
+++ src/Components/VoteOptions/VoteOptions.tsx
@@ -11,9 +11,9 @@ interface VoteOptionsProps {
 const VoteOptions: React.FC<VoteOptionsProps> = ({ onVote, onReset, canReset }) => {
   return (
     <div className={css.container}>
-      <button className={css.button} onClick={() => onVote('good')}>Good</button>
-      <button className={css.button} onClick={() => onVote('neutral')}>Neutral</button>
-      <button className={css.button} onClick={() => onVote('bad')}>Bad</button>
+      <button className={css.button} onClick={() => onVote('good')}>good</button>
+      <button className={css.button} onClick={() => onVote('neutral')}>neutral</button>
+      <button className={css.button} onClick={() => onVote('bad')}>bad</button>
       {canReset && (
         <button className={`${css.button} ${css.reset}`} onClick={onReset}>
           Reset
